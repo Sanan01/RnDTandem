f433c04a571c1fd86358f17de015a6a30dc3a9fe
Sanan Baig <sananbaig@gmail.com>
2024-07-03 20:05:51 +0500

    Project RnD started

--- App.tsx
+++ App.tsx
@@ -1,118 +1,163 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
+  View,
   StyleSheet,
   Text,
-  useColorScheme,
-  View,
+  PermissionsAndroid,
+  Platform,
 } from 'react-native';
+import Geolocation from '@react-native-community/geolocation';
+import {MAPBOX_ACCESS_TOKEN} from './mapboxConfig';
+import MapboxGL from '@rnmapbox/maps';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+MapboxGL.setAccessToken(MAPBOX_ACCESS_TOKEN);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+const App = () => {
+  const [routeCoordinates, setRouteCoordinates] = useState([]);
+  const [currentLocation, setCurrentLocation] = useState(null);
+  const [dest] = useState([67.15551060199695, 24.894371358061537]);
+  const [distance, setDistance] = useState(null);
+  const [duration, setDuration] = useState(null);
+  const [currentSpeed, setCurrentSpeed] = useState(null); // New state for speed
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
+  useEffect(() => {
+    requestLocationPermission();
+  }, []);
+
+  const requestLocationPermission = async () => {
+    if (Platform.OS === 'ios') {
+      getCurrentLocation();
+    } else {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
           {
-            color: isDarkMode ? Colors.white : Colors.black,
+            title: 'Location Permission',
+            message: 'This app needs access to your location',
+            buttonNeutral: 'Ask Me Later',
+            buttonNegative: 'Cancel',
+            buttonPositive: 'OK',
           },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          getCurrentLocation();
+        } else {
+          console.log('Location permission denied');
+        }
+      } catch (err) {
+        console.warn(err);
+      }
+    }
+  };
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const getCurrentLocation = () => {
+    Geolocation.watchPosition(position => {
+      console.log('Getting location', position);
+      const {latitude, longitude, speed} = position.coords;
+      setCurrentLocation([longitude, latitude]);
+      fetchRoute([longitude, latitude], dest);
+      setCurrentSpeed(speed);
+    });
+  };
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const fetchRoute = async (origin, destination) => {
+    const response = await fetch(
+      `https://api.mapbox.com/directions/v5/mapbox/driving/${origin[0]},${origin[1]};${destination[0]},${destination[1]}?geometries=geojson&access_token=${MAPBOX_ACCESS_TOKEN}`,
+    );
+    const data = await response.json();
+    const route = data.routes[0];
+    setRouteCoordinates(route.geometry.coordinates);
+    setDistance(route.distance); // Distance in meters
+    setDuration(route.duration); // Duration in seconds
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <MapboxGL.MapView style={styles.map}>
+        {currentLocation && (
+          <MapboxGL.Camera
+            pitch={45}
+            zoomLevel={14} // Zoom level for a closer view
+            centerCoordinate={currentLocation}
+            animationMode={'flyTo'}
+            animationDuration={2000} // 2 seconds animation duration
+          />
+        )}
+
+        {routeCoordinates.length > 0 && (
+          <MapboxGL.ShapeSource
+            id="routeSource"
+            shape={{
+              type: 'Feature',
+              geometry: {
+                type: 'LineString',
+                coordinates: routeCoordinates,
+              },
+            }}>
+            <MapboxGL.LineLayer id="routeLayer" style={styles.line} />
+          </MapboxGL.ShapeSource>
+        )}
+
+        {currentLocation && (
+          <MapboxGL.PointAnnotation
+            id="currentLocation"
+            coordinate={currentLocation}>
+            <View style={styles.markerContainer}>
+              <View style={styles.marker} />
+            </View>
+          </MapboxGL.PointAnnotation>
+        )}
+
+        <MapboxGL.PointAnnotation id="destination" coordinate={dest}>
+          <View style={styles.markerContainer}>
+            <View style={styles.marker} />
+          </View>
+        </MapboxGL.PointAnnotation>
+      </MapboxGL.MapView>
+
+      <View style={styles.infoContainer}>
+        {distance && duration && (
+          <>
+            <Text>Distance: {(distance / 1000).toFixed(2)} km</Text>
+            <Text>Duration: {(duration / 60).toFixed(2)} mins</Text>
+          </>
+        )}
+        {currentSpeed !== null && (
+          <Text>Speed: {(currentSpeed * 3.6).toFixed(2)} km/h</Text>
+        )}
+      </View>
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  map: {
+    flex: 1,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  markerContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  highlight: {
-    fontWeight: '700',
+  marker: {
+    height: 20,
+    width: 20,
+    backgroundColor: 'red',
+    borderRadius: 10,
+    borderColor: 'white',
+    borderWidth: 2,
   },
+  infoContainer: {
+    position: 'absolute',
+    bottom: 20,
+    left: 20,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    padding: 10,
+    borderRadius: 5,
+  },
+  line: {lineColor: 'blue', lineWidth: 3},
 });
 
 export default App;
---
+++ mapboxConfig.js
@@ -0,0 +1,2 @@
+export const MAPBOX_ACCESS_TOKEN =
+  'pk.eyJ1Ijoic2FuYW5iYWlnIiwiYSI6ImNseTVzOG54dzAycDkya3FyNnlpb2Nvdm8ifQ.CQyIjMi-CLaXWwrrwi8VZA';
